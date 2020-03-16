2a9836eb1c8ce4f762f58750b01e1d57b44c7214
Rachel Schieferstein <violenza@gmail.com>
2020-03-16 10:20:34 -0700

    working through adding database. removing instances from item class. >> >> > >> >> > >> >> Co-authored-by: Rachel Schieferstein <violenza@gmail.com> Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace ToDoList.Models
 {
@@ -12,20 +13,43 @@ namespace ToDoList.Models
     {
       Description = description;
     }
+    public Item(string description, int id)
+    {
+      Description = description;
+      Id = id;
+    }
 
     public static List<Item> GetAll()
     {
-      return _instances;
+      List<Item> allItems = new List<Item> { };
+        MySqlConnection conn = DB.Connection();
+        conn.Open();
+        MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+        cmd.CommandText = @"SELECT * FROM items;";
+        MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+        while (rdr.Read())
+        {
+            int itemId = rdr.GetInt32(0);
+            string itemDescription = rdr.GetString(1);
+            Item newItem = new Item(itemDescription, itemId);
+            allItems.Add(newItem);
+        }
+        conn.Close();
+        if (conn != null)
+        {
+            conn.Dispose();
+        }
+        return allItems;
     }
 
     public static void ClearAll()
     {
-      _instances.Clear();
     }
 
     public static Item Find(int searchId)
     {
-      return _instances[searchId-1];
+      Item placeholderItem = new Item("placeholder item");
+      return placeholderItem;
     }
   }
 }
